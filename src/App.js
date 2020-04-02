7d2a31c6ace5372ca3e6cc3e2ccce4ee0cee4a11
ragasagar <sagarpoudel2052@gmail.com>
2020-04-02 17:07:22 +0530

    Homepage setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Homepage from './pages/homepages/homepage.component';
+
 
 function App() {
   return (
     <div>
+      <Homepage />
     </div>
   );
 }
---
+++ src/pages/homepages/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Directory from '../../components/directory/directory.component.jsx';
+
+
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <Directory />
+        </div>
+    </div>
+)
+
+export default HomePage;
