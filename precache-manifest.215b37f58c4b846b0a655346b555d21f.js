4d5163147b7eb62efc60f1f4efbafa0e342027e7
ragasagar <sagarpoudel2052@gmail.com>
2020-04-02 17:42:25 +0530

    Updates

---
+++ precache-manifest.215b37f58c4b846b0a655346b555d21f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e0424942d6a1bacbd5c596e5b128b0d",
+    "url": "/daraz-collection/index.html"
+  },
+  {
+    "revision": "ade118701f93d3a2826f",
+    "url": "/daraz-collection/static/css/main.214f1567.chunk.css"
+  },
+  {
+    "revision": "f178d124a02fd5336409",
+    "url": "/daraz-collection/static/js/2.7fe920a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/daraz-collection/static/js/2.7fe920a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ade118701f93d3a2826f",
+    "url": "/daraz-collection/static/js/main.07177134.chunk.js"
+  },
+  {
+    "revision": "cddca791ba92a7771fa0",
+    "url": "/daraz-collection/static/js/runtime-main.62f6775e.js"
+  }
+]);
